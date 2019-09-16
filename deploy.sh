2bdeccb3f958aacd31150241aae489ea7d42bd30
Mat Werber <werberm@amazon.com>
2019-09-16 13:49:47 -0400

    update packaged-template name to .yaml

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,14 @@ echo 'Creating CloudFormation package...'
 aws cloudformation package \
   --template-file template.yaml \
   --s3-bucket $DEPLOY_BUCKET \
-  --output-template-file packaged-template.json
+  --output-template-file packaged-template.yaml
 
 # Deploy our changes
 echo 'Deploying CloudFormation package...'
 aws cloudformation deploy \
-  --template-file packaged-template.json \
+  --template-file packaged-template.yaml \
   --stack-name $STACK \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
   --parameter-overrides AnalyticsBucket="$ANALYTICS_BUCKET" AnalyticsKeyPrefix="$ANALYTICS_KEY_PREFIX"
+
+  aws s3 cp template.yaml
